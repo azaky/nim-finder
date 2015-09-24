9a999b49fe2eb955b76605a88f777c33f3160654
azaky <a_zaky003@yahoo.com>
2015-09-24 19:40:30 +0700

    Add analytics

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ uglifyjs --compress --mangle -o "$dist_dir"/nim-finder.required.min.js -- "$dist
 jsobfuscate -o encodeNumbers=false "$dist_dir"/nim-finder.required.min.js > "$dist_dir"/nim-finder.obfuscated.min.js
 
 ## combine with others
-browserify "$dist_dir"/material.min.js "$dist_dir"/chosen.jquery.min.js "$dist_dir"/jquery.twbsPagination.min.js "$dist_dir"/nim-finder.obfuscated.min.js -o "$dist_dir"/nim-finder.bundle.min.js
+browserify "$dist_dir"/material.min.js "$dist_dir"/chosen.jquery.min.js "$dist_dir"/jquery.twbsPagination.min.js "$dist_dir"/nim-finder.obfuscated.min.js "$dist_dir"/analytics.js -o "$dist_dir"/nim-finder.bundle.min.js
 
 ## clean up
 rm "$dist_dir"/nim-finder.required.js
@@ -26,7 +26,7 @@ uglifyjs --compress --mangle -o "$dist_dir"/nim-finder-report.required.min.js --
 jsobfuscate -o encodeNumbers=false "$dist_dir"/nim-finder-report.required.min.js > "$dist_dir"/nim-finder-report.obfuscated.min.js
 
 ## combine with others
-browserify "$dist_dir"/material.min.js "$dist_dir"/nim-finder-report.obfuscated.min.js -o "$dist_dir"/nim-finder-report.min.js
+browserify "$dist_dir"/material.min.js "$dist_dir"/nim-finder-report.obfuscated.min.js "$dist_dir"/material.min.js -o "$dist_dir"/nim-finder-report.min.js
 
 ## clean up
 rm "$dist_dir"/nim-finder-report.required.js
